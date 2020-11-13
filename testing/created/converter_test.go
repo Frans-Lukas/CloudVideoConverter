5c78a831d1f807b0086aada711b0ac99785fdb2a
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-11-13 12:44:17 +0100

    moving around

--- testing/created/converter_test.go
+++ testing/created/converter_test.go
@@ -2,16 +2,16 @@ package created
 
 import (
 	"errors"
+	"github.com/Frans-Lukas/cloudvideoconverter/video_converter"
 	"math/rand"
 	"testing"
 	"time"
 )
-import converter "github.com/Frans-Lukas/cloudvideoconverter/server"
 
 func TestTokenGenerateRandomString(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	str1 := converter.GenerateRandomString()
-	str2 := converter.GenerateRandomString()
+	str1 := video_converter.GenerateRandomString()
+	str2 := video_converter.GenerateRandomString()
 	if str1 == str2 {
 		fatalFail(errors.New("random string generation is not random"))
 	}
