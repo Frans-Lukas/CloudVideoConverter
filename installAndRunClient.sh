c6aae63427a874cafb7d1b2d1e3465f166e77b1f
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-11-20 14:56:37 +0100

    client script added

--- installAndRunClient.sh
+++ installAndRunClient.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 PORT" >&2
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 IP PORT" >&2
   exit 1
 fi
 
 sudo apt-get install wget -y
 sudo apt-get install git -y
-sudo apt-get install ffmpeg -y
 wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-sudo apt-get install git -y
 go get -u google.golang.org/grpc
 git clone https://github.com/Frans-Lukas/CloudVideoConverter.git
 cd CloudVideoConverter
 mkdir localStorage
-go run server/main.go $1
+#download video to localStorage
+go run client/main.go $1 $2
