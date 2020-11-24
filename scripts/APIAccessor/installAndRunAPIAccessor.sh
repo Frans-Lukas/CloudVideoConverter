823807197dc19db670ca010a110fdc2c741f555e
HissPirat <franslukas@gmail.com>
2020-11-24 10:25:07 +0100

    updated scripts

--- scripts/APIAccessor/installAndRunAPIAccessor.sh
+++ scripts/APIAccessor/installAndRunAPIAccessor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 API_IP API_PORT THISIP THISPORT" >&2
+if [ "$#" -ne 5 ]; then
+  echo "Usage: $0 API_IP API_PORT {-add|-remove} THISIP THISPORT " >&2
+  echo "EX: $0 localhost 1337 -add 132.13.3.7 50041 " >&2
   exit 1
 fi
 
--- scripts/APIAccessor/runAPIAccessorWithoutInstall.sh
+++ scripts/APIAccessor/runAPIAccessorWithoutInstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 API_IP API_PORT THISIP THISPORT" >&2
+if [ "$#" -ne 5 ]; then
+  echo "Usage: $0 API_IP API_PORT {-add|-remove} THISIP THISPORT " >&2
+  echo "EX: $0 localhost 1337 -add 132.13.3.7 50041 " >&2
   exit 1
 fi
 cd CloudVideoConverter
 export PATH=$PATH:/usr/local/go/bin
 git pull
-go run api-gateway/client/main.go $1 $2 $3 $4
+go run api-gateway/client/main.go $1 $2 $4 $5 $3
