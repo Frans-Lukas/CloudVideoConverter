e32be472abd1876b4dca7ea56b637e4d7c482554
HissPirat <franslukas@gmail.com>
2020-11-24 09:28:32 +0100

    succesfully compiled api gateway

--- api-gateway/gateway_interface.go
+++ api-gateway/gateway_interface.go
@@ -3,7 +3,6 @@ package api_gateway
 import (
 	"context"
 	api_gateway "github.com/Frans-Lukas/cloudvideoconverter/api-gateway/generated"
-	videoconverter "github.com/Frans-Lukas/cloudvideoconverter/generated"
 )
 
 type APIGatewayServer struct {
@@ -16,15 +15,18 @@ func CreateNewServer() APIGatewayServer {
 }
 
 func (serv *APIGatewayServer) AddServiceEndpoint(
-	ctx context.Context, in *api_gateway.,
-) (*videoconverter.UploadTokenResponse, error) {
+	ctx context.Context, in *api_gateway.ServiceEndPoint,
+) (*api_gateway.AddedServiceEndPoint, error) {
+	return nil, nil
+}
 
-	rpc
-	AddServiceEndpoint(ServiceEndPoint)
-	returns (AddedServiceEndPoint){}
-	rpc
-	DisableServiceEndpoint(DisableServiceEndPoint)
-	returns (DisabledServiceEndPoint){}
-	rpc
-	GetActiveServiceEndpoints(ServiceEndPointsRequest)
-	returns (ServiceEndPointList){}
+func (serv *APIGatewayServer) DisableServiceEndPoint(
+	ctx context.Context, in *api_gateway.DisableServiceEndPoint,
+) (*api_gateway.DisabledServiceEndPoint, error) {
+	return nil, nil
+}
+func (serv *APIGatewayServer) GetActiveServiceEndpoints(
+	ctx context.Context, in *api_gateway.ServiceEndPointsRequest,
+) (*api_gateway.ServiceEndPointList, error) {
+	return nil, nil
+}
