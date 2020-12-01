0cc5ee6dbd5b772f393ef6d0671a05f7122d324a
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-12-01 17:16:18 +0100

    fixed wrong token

--- load-balancer/testStorage/testClient.go
+++ load-balancer/testStorage/testClient.go
@@ -33,6 +33,6 @@ func main() {
 		}
 	}
 	println("downloading part... ")
-	client.DownloadSpecificParts("cyD86dl4yHJ91govrQWzAAAAB")
+	client.DownloadSpecificParts("cyD86dl4yHJ91govrQWz")
 	println("downloaded part!")
 }
