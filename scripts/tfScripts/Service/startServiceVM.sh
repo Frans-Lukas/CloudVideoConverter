d8293254c728f319fe62ba838f7a108cac86db4f
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-12-28 15:50:58 +0100

    updated path

--- scripts/tfScripts/Service/startServiceVM.sh
+++ scripts/tfScripts/Service/startServiceVM.sh
@@ -4,6 +4,6 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-cd CloudVideoConverter/scripts/tfScripts/Service
+cd /home/group9/CloudVideoConverter/scripts/tfScripts/Service
 sudo terraform init
 sudo terraform apply -input=false -auto-approve -var 'instance_count='$1''
