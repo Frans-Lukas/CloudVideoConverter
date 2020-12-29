632cb91d0c2a108d6cdc36fff092f4f970703147
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-12-29 14:51:49 +0100

    added self delete script

--- scripts/VMDeleteSelf.sh
+++ scripts/VMDeleteSelf.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+gcloud auth activate-service-account fast-blueprint-296210@appspot.gserviceaccount.com --key-file=/tmp/SSDNIA.json
 export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
 export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
 gcloud --quiet compute instances delete $NAME --zone=$ZONE
