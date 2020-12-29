d3a04b258d2d957330ada093b8e278ccbe7568a6
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-12-29 14:04:23 +0100

    added spawning pool control

--- scripts/tfScripts/SpawningPool/killAllSpawningPools.sh
+++ scripts/tfScripts/SpawningPool/killAllSpawningPools.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "The number of arguments passed is incorrect"
-    exit 1
-fi
 cd scripts/tfScripts/SpawningPool
 export PATH=$PATH:/Home/staff/pirat/IdeaProjects/CloudVideoConverter/scripts/tfScripts/LoadBalancer
 terraform init
-terraform apply -input=false -auto-approve -var 'instance_count='$1''
+terraform destroy -input=false -auto-approve
