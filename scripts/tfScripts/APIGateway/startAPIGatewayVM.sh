47b2b0f344efb2e0a26cd5ae6487c475ed0c810c
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-12-30 15:47:34 +0100

    fixed ssh-agent

--- scripts/tfScripts/APIGateway/startAPIGatewayVM.sh
+++ scripts/tfScripts/APIGateway/startAPIGatewayVM.sh
@@ -5,6 +5,7 @@ sudo terraform init
 sudo terraform apply -input=false -auto-approve -var 'instance_count='1'' -var-file="/home/group9/CloudVideoConverter/scripts/tfScripts/variables.tfvars"
 gcloud auth activate-service-account fast-blueprint-296210@appspot.gserviceaccount.com --key-file=/tmp/SSDNIA.json
 IP=$(gcloud compute instances describe api-gateway-0 --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=europe-north1-a)
+echo "IP='$IP'"
 pkill ssh-agent
 eval "$(ssh-agent)"
 sudo ssh-add /tmp/id_rsa
