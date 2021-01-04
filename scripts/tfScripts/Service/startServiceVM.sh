527eac52bc34dcd5a0060d46748aee63743b3df6
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2021-01-04 11:45:59 +0100

    removed mkdir

--- scripts/tfScripts/Service/startServiceVM.sh
+++ scripts/tfScripts/Service/startServiceVM.sh
@@ -3,8 +3,6 @@ if [ $# -ne 1 ]; then
     echo "The number of arguments passed is incorrect"
     exit 1
 fi
-cd /home/group9/CloudVideoConverter
-
-cd /scripts/tfScripts/Service
+cd /home/group9/CloudVideoConverter/scripts/tfScripts/Service
 sudo terraform init
 sudo terraform apply -input=false -auto-approve -var 'instance_count='$1'' -var-file="../variables.tfvars"
