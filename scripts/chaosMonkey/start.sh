aecc35c647a2de64770093bb3095ced6be3c4bed
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2021-01-06 15:22:13 +0100

    started chaos monkey script

--- scripts/chaosMonkey/start.sh
+++ scripts/chaosMonkey/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcloud auth activate-service-account fast-blueprint-296210@appspot.gserviceaccount.com --key-file=/tmp/SSDNIA.json
-gcloud compute instances list
+instances=$(gcloud compute instances list --format='table(name)')
 #gcloud compute instances delete
