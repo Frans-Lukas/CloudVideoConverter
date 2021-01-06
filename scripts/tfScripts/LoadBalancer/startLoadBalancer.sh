9204590412d5cd8c2b1a4f0d131495797ac68431
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2021-01-06 16:25:08 +0100

    updated ip port of script

--- scripts/tfScripts/LoadBalancer/startLoadBalancer.sh
+++ scripts/tfScripts/LoadBalancer/startLoadBalancer.sh
@@ -5,10 +5,11 @@ sudo git pull
 sudo chmod -R +x .
 gcloud auth activate-service-account fast-blueprint-296210@appspot.gserviceaccount.com --key-file=/tmp/SSDNIA.json
 IP=$(gcloud compute instances describe api-gateway-0 --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=europe-north1-a)
+MY_IP=$(curl https://ipinfo.io/ip)
 
 while true
 do
-    /usr/local/go/bin/go run load-balancer/server/main.go 50052 "$IP:50051"
+    /usr/local/go/bin/go run load-balancer/server/main.go MY_IP 50052 50054 "$IP:50051"
     echo "Server 'load balancer' crashed with exit code $?.  Respawning.."
     sleep 1
 done
