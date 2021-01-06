be44b3d4070a42a42adc8dae473faf345fda38b5
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2021-01-06 15:38:54 +0100

    chaos monkey progress

--- scripts/chaosMonkey/start.sh
+++ scripts/chaosMonkey/start.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 gcloud auth activate-service-account fast-blueprint-296210@appspot.gserviceaccount.com --key-file=/tmp/SSDNIA.json
-instances="$(gcloud compute instances list --format='table(name)')"
-while IFS= read -r line
-do
-    echo $line
-done < <(printf '%s\n' "$instances")
+while true; do
+    instances="$(gcloud compute instances list --format='table(name)')"
+    while IFS= read -r line
+    do
+        if [[ "$line" == "START" ]]; then
+            echo $line
+        elif [["$line" == *"load-balancer"*]]; then
+            echo "killing '$line'"
+        elif [["$line" == *"service-provider"*]]; then
+            echo "killing '$line'"
+        elif [["$line" == *"api-gateway"*]]; then
+            echo "ignoring '$line'"
+        elif [["$line" == *"spawning-pool"*]]; then
+            echo "ignoring '$line'"
+        fi
+    done < <(printf '%s\n' "$instances")
+    sleep 15
+done
 #gcloud compute instances delete
