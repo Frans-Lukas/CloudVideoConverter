3042fe5dee887c2110eded44e44275f65a3c58c8
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2021-01-08 15:25:07 +0100

    removal of finished parts..

--- load-balancer/load_balancer_interface.go
+++ load-balancer/load_balancer_interface.go
@@ -23,7 +23,7 @@ import (
 )
 
 const tokenLength = 20
-const tokenTimeOutSeconds = 60 * 1
+const tokenTimeOutSeconds = 60 * 10
 const megaByte = 1000000
 const sizeLimit = megaByte * 1
 const NumberOfMovingAvgsToAccountFor = 15
